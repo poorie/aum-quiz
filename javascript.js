93491cdc561510a41e2ec05aa5af95d787b2407a
Gokuchan <73154359+mshifflette@users.noreply.github.com>
2021-09-27 15:08:36 -0700

    have quiz portion of the game working

--- javascript.js
+++ javascript.js
@@ -2,36 +2,136 @@ var pageTitle = document.getElementById("page-title");
 var descriptionHome = document.getElementById("description-home");
 var startButton = document.getElementById("start-button");
 var highScoreLink = document.getElementById("highscore-link");
-var timer = document.getElementById("timer");
+var timer;
+var timeContainer = document.getElementById("timer-container");
 var pokemonImg = document.getElementById("pokemon-image");
 var questionContainer = document.getElementById("question-container");
 var homePage = document.getElementById("homepage");
+var questionIndex = 0;
+var optionsIndex = -1;
+var questionButtons = document.getElementsByClassName(".option-btn");
+var optionButton1 = document.getElementById("option1");
+var optionButton2 = document.getElementById("option2");
+var optionButton3 = document.getElementById("option3");
+var optionButton4 = document.getElementById("option4");
+var timeLeft = 60;
+var playerScore = 0;
+var scorePage = document.getElementById("scorepage");
 
-var questions = [ {
-    pokemonImgQuestion: pokemonImg.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png", 
+function showStartPage() {
+homePage.classList.remove("hide");
+}
+function showGamePage() {
+questionContainer.classList.remove("hide");
+}
+function showScorePage() {
+scorePage.classList.remove("hide");
+questionContainer.classList.add("hide");
+}
+
+function setScore(){
+    playerScore = timeLeft;
+}
+function displayScore(){
+    var scoreContent = document.getElementById("playerscore");
+    scoreContent.textContent = playerScore;
+
+}
+
+
+function endGame(){
+    setScore();
+    timeContainer.classList.add("hide");
+    showScorePage();
+    displayScore();
+}
+
+function optionsBtnPressed(newIndex){
+    optionsIndex = newIndex;
+    checkRightOrWrong();
+}
+
+function checkRightOrWrong() {
+    var correct = questions[questionIndex].options[optionsIndex].correct;
+    if (correct){
+        // Logic for right or wrong
+    }else{
+        timeLeft -= 10;
+    }
+    
+    alert(correct);
+    questionIndex++;
+    
+    if(questionIndex == questions.length){
+        endGame();
+    }
+    setupGameRound();
+    
+}
+
+var questions = [{
+    pokemonImgSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png", 
     options: [
         {content: "snorlax", correct: true},
-        {content: "suresnacks", correct: false}
+        {content: "suresnacks", correct: false},
+        {content: "sumosmash", correct: false},
+        {content: "sleepoi", correct: false}
+    ]
+},
+{
+    pokemonImgSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png", 
+    options: [
+        {content: "plantis", correct: true},
+        {content: "plantra", correct: false},
+        {content: "bellsprout", correct: false},
+        {content: "bellsnort", correct: false}
+    ]
+},
+{
+    pokemonImgSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/145.png", 
+    options: [
+        {content: "electro", correct: true},
+        {content: "thunderhawk", correct: false},
+        {content: "zipdas", correct: false},
+        {content: "zapdos", correct: false}
     ]
 }
 ]
- function nextPage (
-     
- )
+// Purpose is to set the ui before each round
+function setupGameRound() {
+    optionButton1.innerText = questions[questionIndex].options[0].content;
+    optionButton2.innerText = questions[questionIndex].options[1].content;
+    optionButton3.innerText = questions[questionIndex].options[2].content;
+    optionButton4.innerText = questions[questionIndex].options[3].content;
+    pokemonImg.src = questions[questionIndex].pokemonImgSrc;
+    
 
-function game() {
-    
-    
+    // if option[index].correct
+    //     updateUi true or false
+}
+
+function updateTimer() {
+    if(timeLeft <= 0){
+        clearInterval(timeLeft);
+        endGame();
+        return;
+        
+    }
+    document.getElementById("progressBar").value = 60 - timeLeft;
+    timeLeft--;
+
+      
 }
 
 
 startButton.addEventListener("click",function(){
+    
     // start button must replace pageTitle with image of pokemon
     homePage.classList.add("hide");
-    questionContainer.classList.remove("hide");
-    
-   
-    game();
+    showGamePage();
+    timer = setInterval(updateTimer, 1000);
+    setupGameRound();
+      
 })
 
 // start button must also replace start button with a list of buttons that are the options
